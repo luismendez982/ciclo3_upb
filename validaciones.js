955f774322e55ff8c8b4a6a28f0b08b162c1877a
luismendez982 <lamendezs@libertadores.edu.co>
2021-09-22 07:45:21 -0500

    sprint 2 validacion js

---
+++ validaciones.js
@@ -0,0 +1,13 @@
+function validar_nombre_usuario(string){
+
+}
+function validar_edad_usuario(string){
+
+}
+function validar_contrasena(string){
+
+}
+
+module.exports.validar_nombre_usuario=validar_nombre_usuario;
+module.exports.validar_edad_usuario=validar_edad_usuario;
+module.exports.validar_contrasena=validar_contrasena;
